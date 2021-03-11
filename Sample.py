77ee52e3ff062940b20f6b99b002744e03a6c0c9
mallikarjuna1992 <mallikarjuna.rpgle@gmail.com>
2021-03-11 14:37:05 +0530

    first commit

--- Sample.py
+++ Sample.py
@@ -1,4 +1,14 @@
-a=10
-b=20
-c=a+b
-print(c)
+import pymysql 
+
+conn = pymysql.connect(user='root',password='',host='localhost',database='python_10am')
+
+#print(conn)
+cur=conn.cursor()
+
+#cur.execute('create database python_10am')
+
+#cur.execute('create table std_details(std_name varchar(20),std_email varchar(25),std_course varchar(15),std_fee int(5),std_mobile varchar(10))')
+print(cur.execute('show tables'))
+#print(cur.fetchall())
+print(cur.fetchone())
+conn.close()
